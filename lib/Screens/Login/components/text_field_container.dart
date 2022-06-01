849f54024c10ba5b496007b35ed615ca23067d53
quyen1102 <quyen.ttq.1102@gmail.com>
2022-06-01 16:47:19 +0700

    upgrade screen

--- lib/Screens/Login/components/text_field_container.dart
+++ lib/Screens/Login/components/text_field_container.dart
@@ -11,14 +11,22 @@ class TextFieldContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    Color _colorShadowTextField = Colors.black38;
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 5),
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 3),
       width: size.width * 0.82,
       decoration: BoxDecoration(
-        color: textFieldColor,
-        borderRadius: BorderRadius.circular(29),
-      ),
+          color: textFieldColor.withOpacity(0.74),
+          borderRadius: BorderRadius.circular(29),
+          boxShadow: [
+            BoxShadow(
+              color: _colorShadowTextField,
+              spreadRadius: 0.5,
+              blurRadius: 3,
+              offset: const Offset(0, 5),
+            )
+          ]),
       child: child,
     );
   }
--- lib/constans.dart
+++ lib/constans.dart
@@ -4,4 +4,5 @@ const kPrimaryColor = Color(0xFF363062);
 const kPrimaryLightColor = Color(0xFF4D4C7D);
 const textColorLight = Color(0xFFFFDAAF);
 const textColor = Color(0xFFFFCC8F);
+const textColorWhite = Color(0xFFFFFFFF);
 const textFieldColor = Color(0xFFDBDFFD);
